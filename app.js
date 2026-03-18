47e513d7ee96e76e19f405696daffe0853368d68
Mehul Morker <49813671+mehulmorker@users.noreply.github.com>
2026-03-18 14:54:24 +0530

    Change server port from 3000 to 8000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Jenkins CI/CD 🚀");
